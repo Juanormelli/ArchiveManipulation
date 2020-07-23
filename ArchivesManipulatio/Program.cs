a192323726d9050b1e023b3426ce9b696d330c29
Juan Ormelli <juan_ormelli@hotmail.com>
2020-07-22 21:33:01 -0300

    Initial commit

---
+++ ArchivesManipulatio/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace ArchivesManipulatio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path");
+            string sourcefilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcefilePath);
+                
+                string sourcefolderPath = Path.GetDirectoryName(sourcefilePath);
+                string targetfolderPath = sourcefolderPath + @"\out";
+                string targetfilePath = targetfolderPath + @"Summary.csv";
+
+                Directory.CreateDirectory(targetfolderPath);
+
+                using (StreamWriter sw= File.AppendText(targetfilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] folder = line.Split(";");
+
+                        string name = folder[0];
+                        double price = double.Parse(folder[1]);
+                        int quantity = int.Parse(folder[2]);
+
+                        Product produc = new Product(name, price, quantity);
+
+                        sw.WriteLine(produc.Name + ", " + produc.total());
+
+
+
+                    } 
+                }
+            }
+            catch(Exception e )
+            {
+                Console.WriteLine("Error"+ e.Message);
+            }
+        }
+    }
+}
